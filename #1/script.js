c92bf4042e2ff4a3e72b883585b23434945da067
Santi14-1 <70587464+Santi14-1@users.noreply.github.com>
2026-06-12 16:56:55 -0300

    Delete #1 directory

--- #1/script.js
+++
@@ -1,7 +0,0 @@
-alert("Hello, World!");
-confirm("¿Estás seguro de que quieres continuar?"); //respuesta = booleano
-prompt("¿Cuál es tu nombre?", "PEPE"); //respuesta = string
-
-//se pueden guardar las variable singresadas por el usuario en un prompt
-//let nombre = prompt("¿Cuál es tu nombre?", "PEPE");
-//alert("Hola, " + nombre + "!");
